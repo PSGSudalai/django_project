072a9e13cbf0b19ec559af02eed5edbaa9329cbd
PSGSudalai <sudalaimuthug397@gmail.com>
2024-07-02 17:24:58 +0530

    create

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home,register,collections,collectionview,product_Details
+from .views import home,register,collections,collectionview,product_Details,login,logout
 urlpatterns=[
     path('home/',home,name="home"),
+    path('login/',login,name="login"),
+    path('logout/',logout,name="logout"),
     path('register/',register,name="register"),
     path('collections/',collections,name="collection"),
     path('category/<str:name>/',collectionview,name="collection"),
